91a85a0064dc54e6999512d235601ebd27b7d549
vijaysan5 <am.sangavi@gmail.com>
2026-06-25 18:21:29 +0530

    Add files via upload

--- NLP and WordCloud/extra work.py
+++ NLP and WordCloud/extra work.py
@@ -1,4 +1,4 @@
-
+""" 
 
 import pandas as pd
 import numpy as np
@@ -59,4 +59,54 @@ plt.ylabel("Sales")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+plt.show() """
+
+
+
+
+
+
+
+print("----------------------------------------------------------------------")
+print("----------------------------------------------------------------------")
+
+# Dimension
+
+# from sklearn.decomposition import PCA
+# from sklearn.preprocessing import StandardScaler
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# df=pd.read_csv("dataset/dim_reduction_dataset.csv")
+# X=df.drop(columns=["target"])
+# # print(X)
+# xs=StandardScaler().fit_transform(X)
+# print(xs)
+# pca=PCA(n_components=2)
+# xp=pca.fit_transform(xs)
+# print(xp)
+# plt.scatter(xp[:,0],xp[:,1])
+# plt.show() 
+
+
+#Association rule mining
+
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori,association_rules
+
+df=pd.read_csv("dataset/supermarket_transactions.csv")
+print(df)
+transactions=df["Items"].apply(lambda x:x.split(","))
+print(transactions)
+
+
+te=TransactionEncoder()
+te_data=te.fit_transform(transactions)
+df_encoded=pd.DataFrame(te_data,columns=te.columns_)
+print(df_encoded)
+
+freq=apriori(df_encoded,min_support=0.05,use_colnames=True)
+rule=association_rules(freq,metric="lift",min_threshold=1)
+
+print(rule[["antecedents","consequents","support","confidence","lift"]]
+      .sort_values(by="lift",ascending=False).head(10))
