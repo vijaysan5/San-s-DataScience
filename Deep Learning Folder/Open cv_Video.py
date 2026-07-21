e9af8cd9f241468d5f9d9552528226f09bcc1e2b
vijaysan5 <am.sangavi@gmail.com>
2026-07-21 17:26:25 +0530

    Add files via upload

--- Deep Learning Folder/Open cv_Video.py
+++ Deep Learning Folder/Open cv_Video.py
@@ -1,2 +1,25 @@
 import cv2
 import os
+
+video = "D:\Sangavi A\San's  DataScience Folder\Deep Learning Folder\Video.mp4"
+
+output = "frames"
+os.makedirs(output, exist_ok=True)
+
+capture = cv2.VideoCapture(video)
+
+count = 0
+
+while True:
+    ret, frame = capture.read()
+
+    if not ret:
+        break
+
+    fline_name = os.path.join(output, f"frame_{count:04d}.jpg")
+    cv2.imwrite(fline_name, frame)
+
+    count += 1
+
+capture.release()
+print(f"Extracted {count} Frames.")
