eef23bb30960a494d22af7f6020e1f0f92c3f48b
vijaysan5 <am.sangavi@gmail.com>
2026-07-29 18:48:42 +0530

    Add files via upload

--- Deep Learning Folder/AI/Text Mining Process_use Dataset.py
+++ Deep Learning Folder/AI/Text Mining Process_use Dataset.py
@@ -1,23 +1,29 @@
+# Get a dataset and use NLP :
+
 import pandas as pan
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-Dataset = pan.read_csv("San's  DataScience Folder/001 ds excel csv files/fake reviews dataset.csv")
+Dataset = pan.read_csv("000 ds csv files/copy fake reviews dataset.csv")
+# print(Dataset)
+
 Labs = [
     "CG",
+    "CG",
+    "OR",
     "OR"
 ]
 
 vector = CountVectorizer()
-Trans = vector.transform(Dataset)
+Trans = vector.fit_transform(Dataset)
 
 MultiNB = MultinomialNB()
 MultiNB.fit(Trans, Labs)
 
-Test = ["Love this! Well made, sturdy, and very comfortable.  I love it!Very pretty"]
+Test = ["Excellent quality product. Perfect for my ccozinha."]
 
-T_Data = vector.fit_transform(Test)
+T_Data = vector.transform(Test)
 Predict = MultiNB.predict(T_Data)
 
 print("Test line :", Test[0])
