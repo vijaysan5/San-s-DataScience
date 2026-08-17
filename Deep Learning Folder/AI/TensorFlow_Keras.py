23ed145b49f86d46c9f763dae6d67361b3e85b04
vijaysan5 <am.sangavi@gmail.com>
2026-08-17 17:18:09 +0530

    Add files via upload

--- Deep Learning Folder/AI/TensorFlow_Keras.py
+++ Deep Learning Folder/AI/TensorFlow_Keras.py
@@ -36,6 +36,7 @@ tf_model = tf.keras.Sequential([
     tf.keras.layers.Dense(1, activation="sigmoid")
 ])
 
+"Compile :---"
 tf_model.compile(
     optimizer = "adam",
     loss = "binary_crossentropy",
@@ -48,3 +49,34 @@ tf_model.fit(ab_train, xy_train, epochs = 10, batch_size = 32)
 loss, accuracy = tf_model.evaluate(ab_test, xy_test)
 
 print("Accuracy :===>", accuracy)
+
+
+
+
+
+# Tens :===>>><<<
+import pandas as pan
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+ds = pan.read_csv("San's  DataScience Folder/000 ds csv files/Adult csv file.csv")
+
+"Remove Missing Value :"
+ds.replace("?", pan.NA, inplace=True)
+ds.dropna(inplace=True)
+
+"Encode :"
+len = LabelEncoder()
+
+for clmn in ds.columns :
+    if ds[clmn].dtype == "object" :
+        ds[clmn] = len.fit_transform(ds[clmn])
+
+ax = ds.drop(["income"], axis = 1)
+ay = ds["income"]
+
+
+
+
+    
